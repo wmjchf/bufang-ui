107e508ad84706a295012dc15cd4b224e9166adb
明洁 <3284304056@qq.com>
2021-03-13 13:51:06 +0800

    feat: dumi文档配置修改

--- dumi/config/config.js
+++ dumi/config/config.js
@@ -1,7 +1,19 @@
 export default {
-  title: "bufang-ui",
+  title: "不方科技ui",
+  description: `站在风口，猪都会飞`,
   chainWebpack(memo) {
     memo.plugins.delete("copy");
   },
-  mode: "site"
+  mode: "site",
+  outputPath: "dist",
+  navs: [
+    {
+      title: "组件库",
+      path: "/components"
+    },
+    {
+      title: "GitHub",
+      path: "https://github.com/wmjchf/bufang-ui.git"
+    }
+  ]
 };
